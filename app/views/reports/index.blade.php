2f52ac7e3f563607d8bb0e1fe1f32bb2ae21c0d3
James Cole <thegrumpydictator@gmail.com>
2013-12-27 17:09:47 +0100

    Extended the report pages.

---
+++ app/views/reports/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.default')
+@section('breadcrumbs', Breadcrumbs::render('home'))
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <h1>Firefly
+            <small>Reports</small>
+        </h1>
+        <p>
+            @foreach($years as $year)
+            <a href="{{URL::Route('yearreport',$year)}}" class="btn
+            btn-default btn-lg">{{$year}}</a>
+
+            @endforeach
+        </p>
+    </div>
+</div>
+
+
+@endsection
+@section('scripts')
+@endsection
