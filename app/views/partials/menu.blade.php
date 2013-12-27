46bdb48adf2556972e96a7d3264951ba91d43e41
James Cole <thegrumpydictator@gmail.com>
2013-12-27 17:09:55 +0100

    Updated the menu’s

--- app/views/partials/menu.blade.php
+++ app/views/partials/menu.blade.php
@@ -15,18 +15,62 @@
           </a>
         </div>
         <ul class="nav navbar-nav">
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Add
+                    <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                    <li><a href="{{URL::Route('addaccount')}}"><span
+                                class="glyphicon glyphicon-plus"></span> Add
+                            account
+                        </a></li>
+                    <li><a href="{{URL::Route('addbeneficiary')}}"><span
+                                class="glyphicon glyphicon-plus"></span>
+                            Add beneficiary</a></li>
+                    <li><a href="{{URL::Route('addbudget')}}"><span
+                                class="glyphicon
+                                glyphicon-plus"></span> Add budget</a></li>
+                    <li><a href="{{URL::Route('addcategory')}}"><span
+                                class="glyphicon glyphicon-plus"></span>
+                            Add category</a></li>
+                    <li><a href="{{URL::Route('addtransaction')}}"><span
+                                class="glyphicon glyphicon-plus"></span>
+                            Add transaction</a></li>
+                    <li><a href="{{URL::Route('addtransfer')}}"><span
+                                class="glyphicon glyphicon-plus"></span> Add
+                            transfer
+                        </a></li>
+                    </ul>
+                    </li>
+
               <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">Lists <b class="caret"></b></a>
             <ul class="dropdown-menu">
-              <li><a href="{{URL::Route('accounts')}}">Accounts</a></li>
-              <li><a href="{{URL::Route('beneficiaries')}}">Beneficiaries</a></li>
-              <li><a href="{{URL::Route('budgets')}}">Budgets</a></li>
-              <li><a href="{{URL::Route('categories')}}">Categories</a></li>
-              <li><a href="{{URL::Route('transactions')}}">Transactions</a></li>
-              <li><a href="{{URL::Route('transfers')}}">Transfers</a></li>
+              <li><a href="{{URL::Route('accounts')}}"><span class="glyphicon
+              glyphicon-list"></span> Accounts</a></li>
+              <li><a href="{{URL::Route('beneficiaries')}}"><span class="glyphicon
+              glyphicon-list"></span> Beneficiaries</a></li>
+              <li><a href="{{URL::Route('budgets')}}"><span class="glyphicon
+              glyphicon-list"></span> Budgets</a></li>
+              <li><a href="{{URL::Route('categories')}}"><span class="glyphicon
+              glyphicon-list"></span> Categories</a></li>
+              <li><a href="{{URL::Route('transactions')}}"><span class="glyphicon
+              glyphicon-list"></span> Transactions</a></li>
+              <li><a href="{{URL::Route('transfers')}}"><span class="glyphicon
+              glyphicon-list"></span> Transfers</a></li>
             </ul>
-          </li>
-            <li><a href="{{URL::Route('settings')}}">Settings</a></li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle"
+                   data-toggle="dropdown">More <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                    <li><a href="{{URL::Route('settings')}}"><span
+                                class="glyphicon glyphicon-cog"></span>
+                            Settings</a></li>
+                    <li><a href="{{URL::Route('reports')}}"><span
+                                class="glyphicon glyphicon-euro"></span>
+                            Reports</a></li>
+                </ul>
+                </li>
+
         </ul>
 
         <div class="collapse navbar-collapse navbar-ex1-collapse">
