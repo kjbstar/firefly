3908555644525a3cce3e3500e64cae4bdd11736b
James Cole <thegrumpydictator@gmail.com>
2014-01-02 07:40:10 +0100

    Added a forgotten button.

--- app/views/transfers/index.blade.php
+++ app/views/transfers/index.blade.php
@@ -4,6 +4,11 @@
 <div class="row">
     <div class="col-lg-12">
         <h3>All transfers</h3>
+        <p>
+            <a href="{{URL::Route('addtransfer')}}" class="btn
+            btn-default"><span class="glyphicon glyphicon-plus-sign"></span>
+                Add transfer</a>
+        </p>
         {{$transfers->links()}}
         @include('list.transfers')
         {{$transfers->links()}}
