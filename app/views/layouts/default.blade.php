62fd8ade2b56ff88c03a4bacfba680a5a22af1c3
James Cole <thegrumpydictator@gmail.com>
2014-01-24 07:27:56 +0100

    Added charset info.

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -7,6 +7,7 @@
     // {{$title}}
     @endif
     </title>
+      <meta charset="UTF-8" />
       <base href="{{URL::to('/')}}/" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
