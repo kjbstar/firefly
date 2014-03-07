108d43afcc0fee535b2966f64b52fab7004e4253
James Cole <thegrumpydictator@gmail.com>
2014-03-07 23:16:44 +0100

    Extended code to facilitate new compare reports.

--- app/views/reports/index.blade.php
+++ app/views/reports/index.blade.php
@@ -18,8 +18,72 @@
                 </ul>
             </ul>
         @endforeach
-
         @endif
+
+
     </div>
 </div>
+<div class="row">
+    <div class="col-lg-6 col-md-12 col-sm-12">
+        <h4>Compare periods with each other</h4>
+
+        <table class="table">
+            <tr>
+                <th>Compare</th>
+                <th colspan="2">With</th>
+            </tr>
+            <tr>
+                <td>
+                    @if(count($years) > 0)
+                    <select name="year_left" id="year_left" class="form-control">
+                    @foreach($years as $year => $months)
+                        <option label="{{$year}}" value="{{$year}}">{{$year}}</option>
+                    @endforeach
+                    </select>
+                    @endif
+                </td>
+                <td>
+                    @if(count($years) > 0)
+                    <select name="year_right" id="year_right" class="form-control">
+                        @foreach($years as $year => $months)
+                        <option label="{{$year}}" value="{{$year}}">{{$year}}</option>
+                        @endforeach
+                    </select>
+                    @endif
+                </td>
+                <td>
+                    <a href="#" class="btn btn-default" id="year-compare">Compare</a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    @if(count($years) > 0)
+                    <select name="month_left" id="month_left" class="form-control">
+                        @foreach($years as $year => $months)
+                        @foreach($months as $nr => $month)
+                        <option label="{{$month}}, {{$year}}" value="{{$year}}-{{$nr}}">{{$month}}, {{$year}}</option>
+                        @endforeach
+                        @endforeach
+                    </select>
+                    @endif
+                </td>
+                <td>
+                    @if(count($years) > 0)
+                    <select name="month_right" id="month_right" class="form-control">
+                        @foreach($years as $year => $months)
+                        @foreach($months as $nr => $month)
+                        <option label="{{$month}}, {{$year}}" value="{{$year}}-{{$nr}}">{{$month}}, {{$year}}</option>
+                        @endforeach
+                        @endforeach
+                    </select>
+                    @endif
+                </td>
+                <td>
+                    <a href="#" class="btn btn-default" id="month-compare">Compare</a>
+                </td>
+            </tr>
+        </table>
+@stop
+@section('scripts')
+<script src="js/reports.js"></script>
 @stop
