1fe378e69cbd5a2bd3e3074a28604fb24173fa67
James Cole <thegrumpydictator@gmail.com>
2014-03-15 07:34:03 +0100

    More testing!

---
+++ app/tests/ComponentControllerTest.php
@@ -0,0 +1,289 @@
+<?php
+
+class ComponentControllerTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $user = User::where('username', 'test')->first();
+        $this->be($user);
+
+    }
+
+    public function testShowIndex()
+    {
+        Route::enableFilters();
+        $response = $this->call('GET', 'home/budget');
+        $view = $response->original;
+        $this->assertResponseStatus(200);
+        $this->assertEquals('All budgets', $view['title']);
+
+        $count = Auth::user()->components()->where('type','budget')->whereNotNull('parent_component_id')->count();
+        $this->assertCount($count, $view['objects']);
+
+        Route::disableFilters();
+
+    }
+
+    public function testTypeahead()
+    {
+        $response = $this->call(
+            'GET', 'home/budget/typeahead'
+        );
+        $this->assertResponseStatus(200);
+        $this->assertNotNull($response);
+
+        $jsonResponse = $this->client->getResponse()->getContent();
+        $responseData = json_decode($jsonResponse, true);
+        $count = Auth::user()->components()->where('type','budget')->count();
+        $this->assertCount($count, $responseData);
+    }
+
+    public function testShowEmpty()
+    {
+        Route::enableFilters();
+        $response = $this->call('GET', 'home/budget/empty');
+        $view = $response->original;
+        $this->assertResponseStatus(200);
+        $this->assertEquals('Transactions without a budget', $view['title']);
+        $transactions = Auth::user()->transactions()->get();
+        $count = 0;
+        foreach($transactions as $t) {
+            if(is_null($t->budget)) {
+                $count++;
+            }
+        }
+        $this->assertCount($count, $view['transactions']);
+        Route::disableFilters();
+    }
+
+    public function testShowEmptyByMonth()
+    {
+        Route::enableFilters();
+        $response = $this->call('GET', 'home/budget/empty/' . date('Y/m'));
+        $view = $response->original;
+        $this->assertResponseStatus(200);
+        $this->assertEquals('Transactions without a budget', $view['title']);
+        $transactions = Auth::user()->transactions()->inMonth(new \Carbon\Carbon)->get();
+        $count = 0;
+        foreach($transactions as $t) {
+            if(is_null($t->budget)) {
+                $count++;
+            }
+        }
+        $this->assertCount($count, $view['transactions']);
+        Route::disableFilters();
+    }
+
+    public function testAdd()
+    {
+        Route::enableFilters();
+        $response = $this->call('GET', 'home/budget/add');
+        $view = $response->original;
+        $this->assertResponseStatus(200);
+        $this->assertEquals('Add new budget', $view['title']);
+        $this->assertSessionHas('previous');
+        $count = Auth::user()->components()->where('type','budget')->count();
+
+        $this->assertCount($count, $view['parents']);
+        Route::disableFilters();
+
+    }
+
+    public function testEmptyPostAdd()
+    {
+        $this->call('POST', 'home/budget/add');
+        $this->assertResponseStatus(302);
+        $this->assertSessionHas('error');
+        $this->assertRedirectedToRoute('addbudget');
+        $this->assertHasOldInput();
+    }
+
+    public function testPostAdd()
+    {
+        $data = ['name'      => 'InTestComponent', 'type' => 'budget',
+                 'reporting' => 1, 'parent_component_id' => 0];
+
+        $this->call('POST', 'home/budget/add', $data);
+        $this->assertResponseStatus(302);
+        $this->assertRedirectedToRoute('index');
+        $this->assertSessionHas('success');
+    }
+
+    public function testPostWithParentAdd()
+    {
+        $c = Auth::user()->components()->whereNull('parent_component_id')->first();
+        $data = ['name'                => 'Bla.', 'type' => 'budget',
+                 'reporting'           => 1, 'parent_component_id' => $c->id];
+
+        $this->call('POST', 'home/budget/add', $data);
+        $this->assertResponseStatus(302);
+        $this->assertRedirectedToRoute('index');
+        $this->assertSessionHas('success');
+    }
+
+    public function testPostTriggerError()
+    {
+        $c = Auth::user()->components()->whereNotNull('parent_component_id')->first();
+        $data = ['name'                => 'Bla.', 'type' => 'budget',
+                 'reporting'           => 1, 'parent_component_id' => $c->id];
+
+        $this->call('POST', 'home/budget/add', $data);
+        $this->assertResponseStatus(302);
+        $this->assertRedirectedToRoute('addbudget');
+        $this->assertSessionHas('error');
+    }
+
+    public function testEdit()
+    {
+        Route::enableFilters();
+        $component = Auth::user()->components()->first();
+        $response = $this->call(
+            'GET', 'home/budget/' . $component->id . '/edit'
+        );
+        $view = $response->original;
+        $this->assertResponseStatus(200);
+        $this->assertEquals('Edit budget ' . $component->name, $view['title']);
+        $this->assertSessionHas('previous');
+        $this->assertCount(1, $view['parents']);
+        Route::disableFilters();
+
+    }
+
+    public function testWithParentEdit()
+    {
+        Route::enableFilters();
+        $component = Auth::user()->components()->whereNotNull('parent_component_id')->first();
+        $response = $this->call(
+            'GET', 'home/budget/' . $component->id . '/edit'
+        );
+        $view = $response->original;
+        $this->assertResponseStatus(200);
+        $this->assertEquals('Edit budget ' . $component->name, $view['title']);
+        $this->assertSessionHas('previous');
+        $count = Auth::user()->components()->whereNull('parent_component_id')->where('id','!=',$component->id)->where('type','budget')->count() + 1;
+
+            $this->assertCount($count, $view['parents']);
+        Route::disableFilters();
+    }
+
+    public function testPostEdit()
+    {
+        $component = Auth::user()->components()->first();
+        $data = ['name' => 'EditedComponent'];
+        $this->call('POST', 'home/budget/' . $component->id . '/edit', $data);
+        $this->assertResponseStatus(302);
+        $this->assertRedirectedToRoute('index');
+        $this->assertSessionHas('success');
+
+    }
+
+    public function testPostEditWithParent()
+    {
+        $component = Auth::user()->components()->whereNotNull('parent_component_id')->first();
+        $data = ['name'                => 'EditedParentComponent',
+                 'parent_component_id' => $component->parent_component_id];
+        $this->call('POST', 'home/budget/' . $component->id . '/edit', $data);
+        $this->assertResponseStatus(302);
+        $this->assertRedirectedToRoute('index');
+        $this->assertSessionHas('success');
+    }
+
+    public function testPostEditInvalid()
+    {
+        $component = Auth::user()->components()->first();
+        $data = ['name' => null];
+        $this->call('POST', 'home/budget/' . $component->id . '/edit', $data);
+        $this->assertResponseStatus(302);
+        $this->assertRedirectedToRoute('editbudget', $component->id);
+        $this->assertSessionHas('error');
+    }
+
+    public function testPostEditDouble()
+    {
+        $component = Auth::user()->components()->first();
+        $data = ['name' => 'EditedParentComponent'];
+        $this->call('POST', 'home/budget/' . $component->id . '/edit', $data);
+        $this->assertResponseStatus(302);
+        $this->assertRedirectedToRoute('editbudget', $component->id);
+        $this->assertSessionHas('error');
+    }
+
+    public function testDelete()
+    {
+        $component = Auth::user()->components()->first();
+        $response = $this->call(
+            'GET', 'home/budget/' . $component->id . '/delete'
+        );
+        $view = $response->original;
+        $this->assertResponseStatus(200);
+        $this->assertSessionHas('previous');
+        $this->assertEquals(
+            $view['title'], 'Delete budget ' . $component->name
+        );
+        $this->assertEquals($view['object']->name, $component->name);
+
+    }
+
+    public function testPostDelete()
+    {
+        $component = Auth::user()->components()->whereNull('parent_component_id')->first();
+        $this->call('POST', 'home/budget/' . $component->id . '/delete');
+        $this->assertResponseStatus(302);
+        $this->assertRedirectedToRoute('index');
+        $this->assertSessionHas('success');
+    }
+
+    public function testShowOverview()
+    {
+        $component = Auth::user()->components()->first();
+        $response = $this->call(
+            'GET', 'home/budget/' . $component->id . '/overview'
+        );
+        $view = $response->original;
+        $this->assertResponseStatus(200);
+        $this->assertEquals(
+            $view['title'], 'Overview for budget "' . $component->name . '"'
+        );
+        $count = $component->transactions()->count();
+        $this->assertEquals($view['component']->name, $component->name);
+        $this->assertCount($count, $view['transactions']);
+        $this->assertEquals('transactions', $view['display']);
+
+    }
+
+    public function testShowOverviewByMonth()
+    {
+        $component = Auth::user()->components()->first();
+        $response = $this->call(
+            'GET', 'home/budget/' . $component->id . '/overview/' . date('Y/m')
+        );
+        $view = $response->original;
+        $this->assertResponseStatus(200);
+        $this->assertEquals(
+            $view['title'],
+            'Overview for budget "' . $component->name . '" in ' . date('F Y')
+        );
+        $this->assertEquals($view['component']->name, $component->name);
+        $this->assertCount(0, $view['transactions']);
+        $this->assertEquals('transactions', $view['display']);
+    }
+
+    public function testShowOverviewByMonthMontly()
+    {
+        $component = Auth::user()->components()->first();
+        $response = $this->call(
+            'GET', 'home/budget/' . $component->id . '/overview?monthly=true'
+        );
+        $view = $response->original;
+        $this->assertResponseStatus(200);
+        $this->assertEquals(
+            $view['title'], 'Overview for budget "' . $component->name . '"'
+        );
+        $this->assertEquals($view['component']->name, $component->name);
+        $this->assertCount(16, $view['transactions']);
+        $this->assertEquals('months', $view['display']);
+    }
+
+
+}
