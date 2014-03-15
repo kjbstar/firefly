1c5cdce3b17eac177c16ad0b863f63cb8bb45f55
James Cole <thegrumpydictator@gmail.com>
2014-03-15 07:08:10 +0100

    Set up test cases.

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -1,19 +1,46 @@
 <?php
 
-class TestCase extends Illuminate\Foundation\Testing\TestCase {
+/**
+ * TODO every successful POST should have a 'success' flash.
+ * TODO every failed POST should have a 'error' flash.
+ */
+class TestCase extends Illuminate\Foundation\Testing\TestCase
+{
 
-	/**
-	 * Creates the application.
-	 *
-	 * @return Symfony\Component\HttpKernel\HttpKernelInterface
-	 */
-	public function createApplication()
-	{
-		$unitTesting = true;
+    /**
+     * Creates the application.
+     *
+     * @return Symfony\Component\HttpKernel\HttpKernelInterface
+     */
+    public function createApplication()
+    {
+        $unitTesting = true;
 
-		$testEnvironment = 'testing';
+        $testEnvironment = 'testing';
 
-		return require __DIR__.'/../../bootstrap/start.php';
-	}
 
+
+        return require __DIR__ . '/../../bootstrap/start.php';
+
+
+    }
+
+    public function testIsTrue() {
+        $this->assertTrue(true);
+    }
+
+    public static function setUpBeforeClass()
+    {
+        parent::setUpBeforeClass();
+        $test = new self;
+        $test->setUp();
+        $test->seed();
+        echo 1;
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+    }
 }
