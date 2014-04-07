7040c4621b1f6328d6a5dd085f950b80a02312d7
James Cole <thegrumpydictator@gmail.com>
2014-04-07 10:45:20 +0200

    Created a seed to cover more code.

--- app/database/seeds/TestSeedTransfers.php
+++ app/database/seeds/TestSeedTransfers.php
@@ -1,6 +1,7 @@
 <?php
 
-class TestSeedTransfers extends Seeder {
+class TestSeedTransfers extends Seeder
+{
     public function run()
     {
         DB::table('transfers')->delete();
@@ -15,24 +16,43 @@ class TestSeedTransfers extends Seeder {
             $count++;
             for ($i = 0; $i < 2; $i++) {
                 $accountFrom = Account::orderBy(DB::Raw('RAND()'))->first();
-                $accountTo = Account::where('id','!=',$accountFrom->id)->orderBy(DB::Raw('RAND()'))->first();
+                $accountTo = Account::where('id', '!=', $accountFrom->id)->orderBy(DB::Raw('RAND()'))->first();
                 $amount = rand(1, 20);
                 $day = rand(10, 28);
 
                 Transfer::create(
-                    ['user_id'          => $user->id,
-                     'accountfrom_id'       => $accountFrom->id,
-                     'accountto_id'       => $accountTo->id,
-                     'description'      => 'Test Transfer #' . $count,
-                     'amount'           => $amount,
-                     'date'             => $current->format('Y-m-').$day,
+                    ['user_id'        => $user->id,
+                     'accountfrom_id' => $accountFrom->id,
+                     'accountto_id'   => $accountTo->id,
+                     'description'    => 'Test Transfer #' . $count,
+                     'amount'         => $amount,
+                     'date'           => $current->format('Y-m-') . $day,
 
-                     ]
+                    ]
                 );
 
             }
 
             $current->addMonth();
         }
+
+        // add a transfer with some components:
+        $transfer = Transfer::create(
+            ['user_id'        => $user->id,
+             'accountfrom_id' => $accountFrom->id,
+             'accountto_id'   => $accountTo->id,
+             'description'    => 'Test Transfer with components #1',
+             'amount'         => 1,
+             'date'           => date('2013-01-23'),
+
+            ]
+        );
+        $budget = $user->components()->where('type','budget')->first();
+        $ben = $user->components()->where('type','beneficiary')->first();
+        $cat = $user->components()->where('type','category')->first();
+        $transfer->attachComponent($budget);
+        $transfer->attachComponent($ben);
+        $transfer->attachComponent($cat);
+        $transfer->save();
     }
 } 
