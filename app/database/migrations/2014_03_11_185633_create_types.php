7644b61baaa629e424b1645ddef9745ed7979ed4
James Cole <thegrumpydictator@gmail.com>
2014-04-30 09:56:09 +0200

    Updated types.

---
+++ app/database/migrations/2014_03_11_185633_create_types.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTypes extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('types', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->timestamps();
+            $table->string('type', 40);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('types');
+	}
+
+}
--- app/database/migrations/2014_03_11_185634_create_components.php
+++ app/database/migrations/2014_03_11_185634_create_components.php
@@ -17,13 +17,18 @@ class CreateComponents extends Migration {
 			$table->increments('id');
 			$table->timestamps();
             $table->integer('user_id')->unsigned();
+            $table->integer('type_id')->unsigned();
             $table->integer('parent_component_id')->nullable()->unsigned();
-            $table->enum('type', ['budget', 'beneficiary','category']);
+
             $table->string('name', 500);
             $table->boolean('reporting')->default(false);
 
             $table->foreign('user_id')
                 ->references('id')->on('users')
+                ->onDelete('cascade');
+
+            $table->foreign('type_id')
+                ->references('id')->on('types')
                 ->onDelete('cascade');
 
             $table->foreign('parent_component_id')
