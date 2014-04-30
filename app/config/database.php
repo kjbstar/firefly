1f09f5e0e8de7c4d840f4204d180b0f7165816b0
James Cole <thegrumpydictator@gmail.com>
2014-04-30 09:37:25 +0200

    Add default connection.

--- app/config/database.php
+++ app/config/database.php
@@ -7,6 +7,18 @@ return [
 
     'default'    => 'mysql',
 
+    'connections' => [
+        'mysql'      => [
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => '',
+            'username'  => '',
+            'password'  => '',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ],
+    ],
 
     'migrations' => 'migrations',
 
