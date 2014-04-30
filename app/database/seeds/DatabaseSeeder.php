967983e886a54083b187ceb90fa60697220623cd
James Cole <thegrumpydictator@gmail.com>
2014-04-30 14:08:25 +0200

    Updated the Seeder.

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,35 +11,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Eloquent::unguard();
-        // create a user
-        $this->call('TestSeedUser');
+        // create the types
+        $this->call('SeedTypes');
 
-        // create three accounts
-        $this->call('TestSeedAccounts');
-
-        // create six components
-        $this->call('TestSeedComponents');
-
-        // give three of them limits
-        $this->call('TestSeedLimits');
-
-        // create 72 transactions, 3 expenses per month
-        // over 2014 and 2013. (thisyear and prevyear)
-        $this->call('TestSeedExpenses');
-
-        // create 24 transactions, 1 income per month
-        // over 2014 and 2013. (thisyear and prevyear)
-        $this->call('TestSeedIncomes');
-
-        // create 24 transfers, 2 per month.
-        // (back and forth between 2 accounts)
-        $this->call('TestSeedTransfers');
-
-        // create two piggy banks
-        $this->call('TestSeedPiggies');
-
-        // create two predictables.
-        $this->call('TestSeedPredictables');
 
 
     }
