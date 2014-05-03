d3f314aa40fd36f5984c049d3ee1468d37883570
James Cole <thegrumpydictator@gmail.com>
2014-05-03 09:11:43 +0200

    Finally filled in my [todo] things.

--- app/views/predictables/overview.blade.php
+++ app/views/predictables/overview.blade.php
@@ -3,15 +3,33 @@
 @section('content')
 <div class="row">
     <div class="col-lg-12 col-md-12">
-        <h3>Overview for {{$predictable->description}}</h3>
+        <h3>Overview for {{{$predictable->description}}}</h3>
+        <div class="btn-group">
+            <a href="{{URL::Route('editpredictable',$predictable->id)}}" class="btn btn-default"><span class="glyphicon glyphicon-pencil"></span></a>
+            <a href="{{URL::Route('deletepredictable',$predictable->id)}}" class="btn btn-default btn-danger"><span class="glyphicon glyphicon-trash"></span></a>
+        </div>
         <p>
-            Basic intro about predictable.
+            "{{{$predictable->description}}}" triggers the {{$predictable->dayOfMonth()}} day of each month
+            for amounts between {{mf($predictable->minimumAmount(),true)}} and {{mf($predictable->maximumAmount(),true)}}.
         </p>
     </div>
 </div>
 <div class="row">
     <div class="col-lg-6 col-md-6">
-        Hier overview van andere properties van dit predictable.
+        @if(count($predictable->components) > 0)
+        <p>
+            Only transactions with
+            the following meta-data are detected.
+        </p>
+        <table class="table">
+            @foreach($predictable->components as $component)
+            <tr>
+                <td>{{ucfirst($component->type->type)}}</td>
+                <td><a href="{{URL::Route('componentoverview',$component->id)}}">{{{$component->name}}}</a></td>
+            </tr>
+            @endforeach
+        </table>
+        @endif
     </div>
 </div>
 <div class="row">
@@ -21,7 +39,6 @@
 
         <div class="btn-group">
             <a href="{{URL::Route('rescanpredictable',$predictable->id)}}" class="btn btn-default">(re)scan transactions</a>
-            <a href="{{URL::Route('rescanallpredictable',$predictable->id)}}" class="btn btn-default">(re)scan ALL transactions</a>
         </div>
     </div>
 </div>
