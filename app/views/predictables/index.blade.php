6658987a9d83b1accdba4c051338624793c02f1a
James Cole <thegrumpydictator@gmail.com>
2014-05-03 09:11:29 +0200

    Also some extra text here.

--- app/views/predictables/index.blade.php
+++ app/views/predictables/index.blade.php
@@ -4,6 +4,11 @@
 <div class="row">
     <div class="col-lg-12 col-md-12">
         <h3>All predictables</h3>
+        <p class="text-info">
+            If you have predictable expenses or incomes you can create "predictables" for them. Firefly will
+            try to find them and mark them as such. The homepage will show them as well, giving you an idea
+            of which bills you can expect this month. Several charts will include them as well.
+        </p>
         <p>
             <a href="{{URL::Route('addpredictable')}}" class="btn btn-default"><span class="glyphicon glyphicon-plus-sign"></span> Add predictable</a>
         </p>
