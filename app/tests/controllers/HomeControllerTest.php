7e462757f8534ccbd636f88216ab386b4c7d62c6
Sander Dorigo <s@nder.be>
2014-05-08 13:04:04 +0200

    First test that involves the database.

--- app/tests/controllers/HomeControllerTest.php
+++ app/tests/controllers/HomeControllerTest.php
@@ -18,4 +18,17 @@ class HomeControllerTest extends TestCase
         $this->assertRedirectedToRoute('login');
     }
 
+    /**
+     * Redirect user if not logged in.
+     *
+     * @covers HomeController::index()
+     */
+    public function testIndexLogin() {
+        $user = User::whereUsername('admin');
+        $this->be($user);
+
+        $this->action('GET', 'HomeController@index');
+        $this->assertRedirectedToRoute('home');
+    }
+
 }
