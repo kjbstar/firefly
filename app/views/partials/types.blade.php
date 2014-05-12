bbd797d4a9a7ebaffb6979868fed07a78292fc5c
kjbstar <kjbstar@gmail.com>
2014-05-12 08:45:45 +0200

    Types/components: dropdown menus à la place de typehead (qui ne fonctionne même pas). On verra plus tard pour cumuler les 2.

--- app/views/partials/types.blade.php
+++ app/views/partials/types.blade.php
@@ -1,6 +1,6 @@
 <!-- all component types in a loop! -->
 @foreach(Type::allTypes() as $i => $type)
-<div class="form-group">
+<!--<div class="form-group">
     <label for="input{{$type->type}}" class="col-sm-3 control-label">{{ucfirst($type->type)}}</label>
     <div class="col-sm-9">
         <input type="text" value="{{{$prefilled[$type->type] or ''}}}"
@@ -14,5 +14,23 @@
 
                id="input{{$type->type}}" autocomplete="off" />
     </div>
+</div>-->
+
+
+<div class="form-group">
+    <label for="input{{$type->type}}" class="col-sm-3 control-label">{{ucfirst($type->type)}}</label>
+    <div class="col-sm-9">
+        <select name="{{$type->type}}" id="input{{$type->type}}" class="form-control">            
+            @foreach(ComponentHelper::indexList($type) as $i)
+              <option value="{{{$i['name']}}}" @if($prefilled[$type->type] == $i['name']) selected="selected" @endif>{{ $i['name'] }}</option>
+              @foreach($i['children'] as $child)
+              <option value="{{{$child['name']}}}" @if($prefilled[$type->type] == $child['name']) selected="selected" @endif>-- {{ $child['name'] }}</option>
+              @endforeach
+            @endforeach
+        </select>
+    </div>
 </div>
+
+
 @endforeach
+
